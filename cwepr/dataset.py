cc1b139ba5abb4c589cd0488c724a3d663d91712
Pascal Kirchner <pascal.kirchner@web.de>
2018-12-11 11:59:17 +0100

    Moved determination of format to the importer.

--- cwepr/dataset.py
+++ cwepr/dataset.py
@@ -1,55 +1,41 @@
 """Dataset (Container for experimental data and respective metadata)"""
 
+import os.path
 
 import aspecd
+
 import cwepr.importers as importers
-
-
-class UnsupportedDataFormatError(aspecd.dataset.Error):
-    """Exception raised when given data format is not supported by an
-    available importer.
-
-    Attributes
-    ----------
-    message : `str`
-        explanation of the error
-
-    """
-
-    def __init__(self, message=''):
-        super().__init__()
-        self.message = message
 
 
 class Dataset(aspecd.dataset.Dataset):
     """
 
     """
-    supported_formats = {"BES3T": importers.ImporterBES3T}
-
     def __init__(self):
         super().__init__()
 
-    def import_from_file(self, dataformat):
-        """Import data and metadata from a given data format.
+    def import_from_file(self, filename, setformat=None):
+        """Import data and metadata for a given filename.
 
-        The appropriate importer is selected automatically for
-        the respective data format.
+        The appropriate importer automatically checks whether
+        data and metadata files exist, matching a single format
 
          Parameters
          ----------
-         dataformat : 'str'
-             Data format of the raw EPR data.
-             Note: Currently, only Bruker BES3T format is supported!
+         filename : 'str'
+             Path including the filename but not the extension.
 
-         Raises
-         ------
-         UnsupportedDataFormatError
-             Raised when given data format is not supported.
-
+         setformat: 'str'
+             Format of the data and metadata. If none is set the
+             importer tries to automatically determine the format.
          """
-        if dataformat not in self.supported_formats.keys():
-            raise UnsupportedDataFormatError(message=(
-                    "The data format " + dataformat + " is not supported."))
-        super().import_from(self.supported_formats[dataformat])
+
+        self.data = super().import_from(
+            importers.ImporterEPRGeneral(source=filename,
+                                         setformat=setformat))
+
+
+#dts = Dataset()
+#path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+#dts.import_from_file((path+"/Messdaten/1_No1-dunkel"))
 
--- cwepr/importers.py
+++ cwepr/importers.py
@@ -1,16 +1,131 @@
-"""Importer (Preparing raw data for processing)
+"""Importers (Preparing raw data for processing)
 
 This importer is used for raw data provided in the Bruker BES3T data format.
 """
 
 
-import aspecd
+import aspecd.io
+import numpy as np
+import os.path
+
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+
+    pass
+
+
+class UnsupportedDataFormatError(Error):
+    """Exception raised when given data format is not supported by an
+    available importer.
+
+    Attributes
+    ----------
+    message : `str`
+        explanation of the error
+
+    """
+
+    def __init__(self, message=''):
+        super().__init__()
+        self.message = message
+
+
+class NoMatchingFilePairError(Error):
+    """Exception raised when no pair of a data and parameter files
+    is found where the extensions match a single format.
+
+    Attributes
+    ----------
+    message : `str`
+        explanation of the error
+
+    """
+
+    def __init__(self, message=''):
+        super().__init__()
+        self.message = message
+
+
+class ImporterEPRGeneral(aspecd.io.Importer):
+    """Importer super class that determines the correct
+    specialized importer for a format.
+
+     Attributes
+    ----------
+    setformat
+        The format of the data to import. Is set manually or
+        determined automatically.
+    importers_for_formats
+        Map of the specialized importers for different formats.
+
+    Raises
+    ------
+    UnsupportedDataFormatError
+        Raised if a format is set but does not match any of the
+        supported formats
+    NoMatchingFilePairError
+        Raised if no pair of files matching any one supported format
+        can be found. Currently only Bruker BES3T is supported.
+
+    """
+    supported_formats = {"BES3T": [".DTA", ".DSC"]}
+
+    def __init__(self, setformat=None, source=None):
+        super().__init__(source=source)
+        self.setformat=setformat
+        self.importers_for_formats = {"BES3T": ImporterBES3T}
+
+    def _import(self):
+        """Call the correct importer for the data format set.
+        If no format is set it is automatically determined
+        from the given filename.
+
+        Raises
+        ------
+        UnsupportedDataFormatError
+            Raised if a format is set but does not match any of
+            the supported formats
+            """
+        if self.setformat is None:
+            self.setformat = self._find_format()
+        else:
+            if self.setformat not in self.importers_for_formats.keys():
+                raise UnsupportedDataFormatError(("""The following format 
+                is not supported: """+self.setformat))
+        special_importer = self.importers_for_formats[
+            self.setformat](source=self.source)
+        return special_importer.import_into(self.dataset)
+
+    def _find_format(self):
+        """Determine the format of the given filename by checking
+        if a data and metadata file matching any supported
+        format are present.
+
+        Raises
+        ------
+        NoMatchingFilePairError
+            Raised if no pair of files matching any one supported
+            format can be found. Currently only Bruker BES3T
+            is supported.
+
+        """
+        for k, v in self.supported_formats.items():
+            if os.path.isfile((self.source+v[0])) and os.path.isfile(
+                (self.source+v[1])
+            ):
+                return k
+        else:
+            raise NoMatchingFilePairError("""No file pair matching a single
+            format was found for path: """+self.source)
 
 
 class ImporterBES3T(aspecd.io.Importer):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, source=None):
+        super().__init__(source=source)
 
     def _import(self):
-        pass
+        """"""
+        complete_filename = self.source+".DTA"
+        return np.fromfile(complete_filename)
 
