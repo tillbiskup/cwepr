719710c27b60deb73d7d36ea76ccdee692d0e821
Pascal Kirchner <pascal.kirchner@web.de>
2018-12-13 16:53:46 +0100

    Finished and debugged DSC parser, updated docstrings

--- cwepr/importers.py
+++ cwepr/importers.py
@@ -6,6 +6,7 @@ This importer is used for raw data provided in the Bruker BES3T data format.
 
 import os.path
 import numpy as np
+import collections as col
 
 
 import aspecd.io
@@ -161,21 +162,21 @@ class ImporterBES3T(aspecd.io.Importer):
         """
         complete_filename = self.source+".DTA"
         raw_data = np.fromfile(complete_filename)
-        #print(raw_data)
         if not self._are_values_plausible(raw_data):
-            raw_data=raw_data.byteswap()
-        #print(raw_data)
+            raw_data = raw_data.byteswap()
         return raw_data
 
     def import_metadata(self):
         """Import parameter file in BES3T format and
         user created info file.
         """
-        file_info = open(self.source+".info")
-        raw_info_data = file_info.readlines()
+        #file_info = open(self.source+".info")
+        #raw_info_data = file_info.readlines()
+
         file_param = open(self.source+".DSC")
-        raw_param_data = file_param.readlines()
-        return [raw_param_data, raw_info_data]
+        raw_param_data = file_param.read()
+        processed_param_data = ParserDSC().parse_dsc(raw_param_data)
+        return processed_param_data
 
     @staticmethod
     def _are_values_plausible(array):
@@ -198,11 +199,68 @@ class ImporterBES3T(aspecd.io.Importer):
 
 
 class ParserDSC:
-    def __init__(self):
-        pass
-
+    """Parser for a *.DSC parameter file belonging to the BES3T
+    format."""
     def parse_dsc(self, file_content):
-        pass
+        """Main method for parsing a *.DSC file into its
+        three parts and each of the three parts into a dictionary
+
+        Parameters
+        ------
+        file_content: 'str'
+        Content of a complete *.DSC file.
+
+        Returns
+        ------
+        three_parts_processed: 'list'
+        The three parts of the file, each represented by a dictionary
+        containing pairs of keys (parameter names) and values
+        (parameter values) The dictionaries of part one and three are
+        additionally subdivided based on headlines.
+        """
+        three_parts = self._get_three_parts(file_content)
+        three_parts_processed = list()
+        three_parts_processed.append(self._create_dict_p1p3(
+            self._subdivide_part1(three_parts[0]), "\t"
+        ))
+        three_parts_processed.append(self._create_dict_part2(
+            self._subdivide_part2(three_parts[1])
+        ))
+        three_parts_processed.append(self._create_dict_p1p3(
+            self._subdivide_part3(three_parts[2]), " ",
+            delimiter_width=19
+        ))
+        print(three_parts_processed)
+        return three_parts_processed
+
+    @staticmethod
+    def _make_dict_without_structure(processed_file):
+        """Make a dictionary containing all information
+        at the same level, not divided by headlines and sub-headlines
+        for easier mapping
+
+        Parameters
+        ------
+        processed_file: 'list'
+        Content of a complete *.DSC file in processed form, i.e. with
+        headlines and sub-headlines.
+
+        Returns
+        ------
+        dict_without_headlines: 'dict'
+        Dictionary of all key-value pairs belonging to any
+        headline or sub-headline, all on the same level. 
+        """
+        dict_without_structure = col.OrderedDict()
+        for content in processed_file[0].values():
+            for title, entry in content.items():
+                dict_without_structure[title] = entry
+        for title, entry in processed_file[1]:
+            dict_without_structure[title] = entry
+        for content in processed_file[2].values():
+            for title, entry in content.items():
+                dict_without_structure[title] = entry
+        return dict_without_structure
 
     @staticmethod
     def _get_three_parts(file_content):
@@ -223,7 +281,7 @@ class ParserDSC:
         three_parts: 'list'
         The three parts of the file.
         """
-        three_parts = []
+        three_parts = list()
         first_split = file_content.split("#SPL")
         three_parts.append(first_split[0])
         second_split = first_split[1].split("#DSL")
@@ -241,9 +299,9 @@ class ParserDSC:
         containing additional text.
 
         Each subdivision is then transformed into a dict entry
-        with the headline, stripped of the first to characters
+        with the headline, stripped of the first two characters
         (*\t) as key and the remaining information as value.
-        Lines containing an asterisk only are removed.
+        Lines containing an asterisk only, are removed.
 
         Parameters
         ------
@@ -252,61 +310,86 @@ class ParserDSC:
 
         Returns
         ------
-        subparts_clean: 'dict'
+        part1_clean: 'dict'
         All subdivisions from the file with headlines as keys
         and list of lines as values; lines devoid of information
         removed.
         """
-        subparts_descriptor = part1.split("\n*\n")
-        del(subparts_descriptor[0])
-        subparts_clean = {}
-        for part in subparts_descriptor:
-            part_split = part.slit("\n")
-            part_split_clean = []
-            for line in part_split:
-                if line != "*":
-                    part_split_clean.append(line)
-            subparts_clean[part_split_clean[0][2:]] = part_split_clean[1:]
-        return subparts_clean
+        lines = part1.split("\n")
+        part1_clean = col.OrderedDict()
+        current_subpart = list()
+        for line in lines:
+            if "*" in line and "*\t" not in line:
+                continue
+            if "*\t" in line and current_subpart != list():
+                part1_clean[current_subpart[0][2:]] = current_subpart[1:]
+                current_subpart = list()
+            current_subpart.append(line)
+        part1_clean[current_subpart[0][2:]] = current_subpart[1:]
+        return part1_clean
 
     @staticmethod
-    def _create_dict_part1(part1_split):
+    def _create_dict_p1p3(p1p3_split, delimiter, delimiter_width=0):
         """Create a dict from the first part ("Descriptor Information")
-        of a *.DSC file.
+        or third part ("Device Specific Layer") of a *.DSC file.
 
         For every subdivision. lines are split at tabs. The method
         accounts for lines containing only a parameter name with no
         corresponding value.
 
+        Note: The format of part 3 of a *.DSC file does not work with tabs,
+        but rather with with a variable number of spaces.
+
         Parameters
         ------
-        part1_split: 'dict'
-        Preprocessed first part of a file, split into
+        p1p3_split: 'dict'
+        Preprocessed first or third part of a file, split into
         subdivisions with the headline as key. Subdivisions split into
         lines with lines devoid of information removed.
 
+        delimiter: 'str'
+        Delimiter separating parameter name and value in one line.
+
+        delimiter_width: 'int'
+        Used to split at a variable number of spaces (could be used
+        for other delimiters, too, though). The method will count
+        all characters before the first delimiter character and determine
+        the number of delimiter characters from the difference to the value
+        of delimiter width.
+
         Returns
         ------
-        part2_dict: 'dict'
+        p1p3_dict: 'dict'
         Data from the input as pairs of key (subdivision name) and
         value (subdivision info). Each subdivision info is also a dict
         with keys (parameter names) and values (parameter values).
         """
-        part1_dict = {}
-        for title, subpart in part1_split.items():
-            subdict = {}
+        p1p3_dict = col.OrderedDict()
+        for title, subpart in p1p3_split.items():
+            param_dict = col.OrderedDict()
             for line in subpart:
-                line_split = line.split("\t")
-                if len(line_split) == 1:
-                    subdict[line_split[0]] = ""
+                if delimiter_width != 0:
+                    param_count = 0
+                    for character in line:
+                        if character != delimiter:
+                            param_count += 1
+                        else:
+                            break
+                    final_delimiter = (delimiter_width-param_count)*delimiter
                 else:
-                    subdict[line_split[0]] = line_split[1]
-            part1_dict[title] = subdict
-        return part1_dict
+                    final_delimiter = delimiter
+                line_split = line.split(final_delimiter)
+                if line_split[0] != "":
+                    if len(line_split) == 1:
+                        param_dict[line_split[0]] = ""
+                    else:
+                        param_dict[(line_split[0].strip())] = line_split[1]
+            p1p3_dict[title] = param_dict
+        return p1p3_dict
 
     @staticmethod
     def _subdivide_part2(part2):
-        """Preprocess the second part ("Standard parameter
+        """Pre process the second part ("Standard parameter
         layer") of a *.DSC file.
 
         The crude string is split into lines; lines devoid of
@@ -315,6 +398,10 @@ class ParserDSC:
 
         Note: every line containing asterisks is removed as
         these appear only in headlines and as delimiters.
+
+        Note: For reasons that will probably never be known,
+        in this part, parameter names are separated from the
+        values by four spaces instead of a tab.
 
         Parameters
         ------
@@ -328,7 +415,7 @@ class ParserDSC:
         information.
         """
         entries_param = part2.split("\n")
-        entries_clean = []
+        entries_clean = list()
         for line in entries_param:
             if "*" not in line:
                 entries_clean.append(line)
@@ -355,11 +442,56 @@ class ParserDSC:
         Data from the input as pairs of key (parameter name) and
         value (parameter value).
         """
-        part2_dict = {}
+        part2_dict = col.OrderedDict()
         for line in part2_split:
-            line_split = line.split("\t")
-            if len(line_split) == 1:
-                part2_dict[line_split[0]] = ""
-            else:
-                part2_dict[line_split[0]] = line_split[1]
+            line_split = line.split("    ")
+            if line_split[0] != "":
+                if len(line_split) == 1:
+                    part2_dict[line_split[0]] = ""
+                else:
+                    part2_dict[line_split[0]] = line_split[1]
         return part2_dict
+
+    @staticmethod
+    def _subdivide_part3(part3):
+        """Pre process the third part ("Device Specific Layer")
+         of a *.DSC file.
+
+        The crude string is split into subdivisions, which are
+        begun with a headline containing the fragment ".DVC"
+
+        Each subdivision is then transformed into a dict entry
+        with the headline, stripped of the first eight characters
+        (".DVC    ") as key and the remaining information as value.
+        Lines containing asterisks are removed.
+
+        Parameters
+        ------
+        part3: 'str'
+        Raw third part of a file.
+
+        Returns
+        ------
+        subparts_clean: 'dict'
+        All subdivisions from the file with headlines as keys
+        and list of lines as values; lines devoid of information
+        removed.
+        """
+        lines = part3.split("\n")
+        subdivisions = col.OrderedDict()
+        current_subpart = list()
+        for line in lines:
+            if "*" in line:
+                continue
+            if ".DVC" in line and current_subpart != []:
+                if len(current_subpart) == 1:
+                    subdivisions[current_subpart[0][9:]] = []
+                else:
+                    subdivisions[current_subpart[0][9:]] = current_subpart[1:]
+                current_subpart.clear()
+            current_subpart.append(line)
+        if len(current_subpart) == 1:
+            subdivisions[current_subpart[0][9:]] = []
+        elif len(current_subpart) != 0:
+            subdivisions[current_subpart[0][9:]] = current_subpart[1:]
+        return subdivisions
