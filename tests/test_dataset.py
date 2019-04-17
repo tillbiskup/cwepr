23f5b15432221bb76ddfa59c53baf85b4a744ad0
Pascal Kirchner <pascal.kirchner@web.de>
2019-04-17 17:09:14 +0200

    Worked on template and tests.

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -3,6 +3,7 @@
 import unittest
 
 from cwepr import dataset
+import os
 
 
 class TestDataset(unittest.TestCase):
@@ -11,3 +12,24 @@ class TestDataset(unittest.TestCase):
 
     def test_instantiate_class(self):
         pass
+
+
+class TestImport(unittest.TestCase):
+    def setUP(self):
+        pass
+
+    def test_import_dataset(self):
+        path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+        dts = dataset.Dataset()
+        dts.import_from_file((path + "/Messdaten/1_No1-dunkel"))
+
+
+class TestDatasetFactory(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_dataset_factory(self):
+        path = os.path.dirname(os.path.dirname(os.path.dirname(__file__))) + "/Messdaten/1_No1-dunkel"
+        factory = dataset.DatasetFactory()
+        ds = factory.get_dataset(source=path)
+        assert(type(ds) == dataset.Dataset)
