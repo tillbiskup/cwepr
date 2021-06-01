6eb9231a26e1a84ec24409868d92c649aeae78ff
Till Biskup <till@till-biskup.de>
2021-06-01 10:20:34 +0200

    Add project URLs to setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,10 @@ setuptools.setup(
     author='Mirjam Schröder, Pascal Kirchner, Till Biskup',
     author_email='till@till-biskup.de',
     url='https://www.cwepr.de/',
+    project_urls={
+        "Documentation": "https://docs.cwepr.de/",
+        "Source": "https://github.com/tillbiskup/cwepr",
+    },
     packages=setuptools.find_packages(exclude=('tests', 'docs')),
     include_package_data=True,
     license='BSD',
@@ -37,6 +41,7 @@ setuptools.setup(
         "Topic :: Scientific/Engineering",
     ],
     install_requires=[
+        'aspecd>=0.2.0',
         'numpy',
         'scipy',
         'matplotlib',
