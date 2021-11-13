03a3ddeb47ac2067c42efb7c1f9230aaf445bc1f
Till Biskup <till@till-biskup.de>
2021-11-13 20:30:31 +0100

    Update package name

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ with open(os.path.join(os.path.dirname(__file__), '..', 'VERSION')) as \
         version_file:
     release_ = version_file.read().strip()
 
-project = 'cwEPR'
+project = 'cwepr'
 copyright = '2020- Mirjam Schröder, 2018/19 Pascal Kirchner, 2018– Till Biskup'
 author = 'Mirjam Schröder, Pascal Kirchner, Till Biskup'
 
@@ -138,7 +138,7 @@ html_favicon = './cwepr-favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
